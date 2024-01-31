dfa7e27c147827778450fb0221e81891b1538e81
Tou-u <rodrignacio99@hotmail.com>
2024-01-31 19:12:34 -0300

    migrations

--- migrations/0000_glossy_epoch.sql
+++ migrations/0000_glossy_epoch.sql
@@ -6,5 +6,9 @@ CREATE TABLE `session` (
 );
 --> statement-breakpoint
 CREATE TABLE `user` (
-	`id` text PRIMARY KEY NOT NULL
+	`id` text PRIMARY KEY NOT NULL,
+	`username` text NOT NULL,
+	`password` text NOT NULL
 );
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);
--- migrations/0001_bumpy_stick.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE user ADD `username` text NOT NULL;--> statement-breakpoint
-ALTER TABLE user ADD `password` text NOT NULL;--> statement-breakpoint
-CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);
