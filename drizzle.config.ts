fda1046c666b778a636e7466308c2172422c8b39
Tou-u <rodrignacio99@hotmail.com>
2024-01-31 21:09:50 -0300

    configuration to connect to production database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: 'src/lib/schema.ts',
   out: 'migrations',
-  driver: 'libsql',
-  breakpoints: true
+  driver: 'd1',
+  dbCredentials: {
+    wranglerConfigPath: '/home/tou/dev/svelte-lucia-cf-d1/wrangler.toml',
+    dbName: 'sveltedb'
+  }
 } satisfies Config;
